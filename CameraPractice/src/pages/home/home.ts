d5f146362b517d5f7ec5b9af72f7ca6e52fea1ff
Joseph Boscán <joeypc.py@gmail.com>
2019-02-13 02:24:15 -0400

    Agregando función de foto por cámara
    Agregando función de foto por cámara

--- CameraPractice/src/pages/home/home.ts
+++ CameraPractice/src/pages/home/home.ts
@@ -27,6 +27,32 @@ export class HomePage {
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      targetWidth: 600,
+      //targetHeight: 300,
+      saveToPhotoAlbum: true,
+      //allowEdit: true,
+      correctOrientation: true
+    }
+    
+    this.camera.getPicture(options).then(
+      (imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64 (DATA_URL):
+        this.base64Image = 'data:image/jpeg;base64,' + imageData;
+      }, 
+      (err) => {
+        // Handle error
+        this.presentToast('No se ha seleccionado imagen');
+      });
+  }
+
+  getPhotoFromGallery(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
       targetWidth: 600,
       correctOrientation: true
